07f1cf9d75f65192cda374c47571eacc9ffa49fa
IsaikinSergei <akinfest007@gmail.com>
2021-02-26 13:56:54 +0400

    Added FIREBASE service

---
+++ ToDoFire/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by Sergei Isaikin on 26.02.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+}
+
---
+++ ToDoFire/Controllers/TasksViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TasksViewController.swift
+//  ToDoFire
+//
+//  Created by Sergei Isaikin on 26.02.2021.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+
+}
